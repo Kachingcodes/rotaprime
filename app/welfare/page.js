114314b0135e235155b3a0ec42c492ed3f588694
Onyekachi Alozie-Nsirimobi <onyekansirimobi16@gmail.com>
2026-09-21 15:20:43 +0100

    Mobile

--- app/welfare/page.js
+++ app/welfare/page.js
@@ -152,7 +152,7 @@ export default function WelfarePage() {
   };
 
   return (
-    <div className="space-y-6 p-6">
+    <div className="space-y-6 p-4 lg:p-6">
       <WelfareHeader />
 
       <WelfareStats
